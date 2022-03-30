7e7ef6de68563d02bcbf98bf9933da5fc111fba8
TomasUri <urinovic.tomas@gmail.com>
2022-03-30 16:16:01 +0300

    2022-03-29

---
+++ js/2022-03-29.js
@@ -0,0 +1,63 @@
+console.log('-------------Kintamuju inicijavimas-----------');
+
+const one = 1;
+console.log(one);
+const two = 2;
+console.log(two);
+const three = 3;
+console.log(three);
+
+
+console.log('-------------------- 2 ---------------------');
+
+const first = 'one';
+console.log(first);
+const second = 'two';
+console.log(second);
+const third = 'three';
+console.log(third);
+
+
+console.log('-------------------- 3 ---------------------');
+
+const numbers = [12, 21, 35, 44, 56];
+console.log(numbers);
+const cats = [5, 4, 3, 2, 1];
+console.log(cats);
+const dogs = [2, 1, 4, 3, 5];
+console.log(dogs);
+
+
+console.log('-------------------- 4 ---------------------');
+
+const names = ['John, Tom, Nick, Norah, Annie.'];
+console.log(names);
+const cars = ['Audi, BMW, Mercedes, Toyota, Tesla.'];
+console.log(cars);
+const companies = ['Microsoft, Apple, Sony, Xiaomi, Samsung.'];
+console.log(companies);
+
+
+console.log('----------Veiksmai su kintamaisiais-1---------');
+
+console.log('=', one + two + three);
+
+console.log('-------------------- 2 ---------------------');
+
+console.log(`${first} ${second} ${third}`);
+
+console.log('-------------------- 3 ---------------------');
+
+const sumOfNum = numbers[0] - numbers[1] + numbers[2] - numbers[3] + numbers[4];
+console.log('sum =', sumOfNum);
+const numOfCats = cats[0] - cats[1] + cats[2] - cats[3] + cats[4];
+console.log('sum =', numOfCats);
+const numOfDogs = dogs[0] - dogs[1] + dogs[2] - dogs[3] + dogs[4];
+console.log('sum =', numOfDogs);
+
+console.log('-------------------- 4 ---------------------');
+
+
+console.log(`${names}`);
+console.log(`${cars}`);
+console.log(`${companies}`);
